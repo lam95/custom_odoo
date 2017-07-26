c1a95af5cd0e8326dcd996d3345216d36fdb2b33
lam95 <lamnguyen31195@gmail.com>
2017-07-26 12:03:07 +0700

    custom odoo

--- todo_app/todo_model.py
+++ todo_app/todo_model.py
@@ -6,7 +6,7 @@ _logger = logging.getLogger(__name__)
 
 class TodoTask(models.Model): 
     _name = 'todo.task' 
-    _description = 'To-do Task by lamnguyen'
+    _description = 'To-do Task by laamNguyen'
     name = fields.Char('Description', required=True)
     is_done = fields.Boolean('Done?') 
     active = fields.Boolean('Active?', default=True)
@@ -20,5 +20,5 @@ class TodoTask(models.Model):
     @api.multi
     def do_clear_done(self):
         dones = self.search([('is_done', '=', True)])
-        dones.write({'active': False})
+        dones.write({'is_done': False})
         return True
