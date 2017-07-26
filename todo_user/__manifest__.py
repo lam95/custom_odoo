a1bc299d902648dedcc9692c1bd70f3f017fd5b4
lam95 <lamnguyen31195@gmail.com>
2017-07-26 23:38:31 +0700

    edit mail

--- todo_user/__manifest__.py
+++ todo_user/__manifest__.py
@@ -2,7 +2,7 @@
     'name': 'Multiuser To-Do', 
     'description': 'Extend the To-Do app to multiuser.', 
     'author': 'Daniel Reis', 
-    'depends': ['todo_app'], 
+    'depends': ['todo_app', 'mail'], 
 
     'data': [
         'views/todo_task.xml',
--- todo_user/mail/todo_mail.py
+++ todo_user/mail/todo_mail.py
@@ -1,4 +1,4 @@
 from odoo import models 
 class TodoTask(models.Model): 
     _name = 'todo.task' 
-    _inherit = 'mail.thread'
+    _inherit = ['todo.task' ,'mail.thread']
