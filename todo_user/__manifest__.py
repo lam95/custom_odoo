2014705aa2307ea06cb90bc90f4e36e8c16b6194
lam95 <lamnguyen31195@gmail.com>
2017-07-27 16:14:47 +0700

    add demo data

--- todo_user/__manifest__.py
+++ todo_user/__manifest__.py
@@ -7,5 +7,10 @@
     'data': [
         'views/todo_task.xml',
         'security/todo_access_rules.xml',
-    ]
+    ],
+
+    'demo': [
+        'data/todo.task.csv',
+        'data/todo_task.xml',
+    ],
 }
