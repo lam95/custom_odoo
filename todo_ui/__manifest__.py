89aef8e7afedc8a012fffc63438c2be8c3553382
lam95 <lamnguyen31195@gmail.com>
2017-07-31 11:43:48 +0700

    todo menu update

--- todo_ui/__manifest__.py
+++ todo_ui/__manifest__.py
@@ -2,5 +2,11 @@
     'name': 'User interface  improvements to the To-Do app',
     'description': 'User friendly features',
     'author': 'Lam95',
-    'depends': ['todo_user']
+    'depends': ['todo_user'],
+
+    'data': [
+        #'security/ir.model.access.csv',
+        'views/todo_view.xml',
+        'views/todo_menu.xml',
+    ],
 }
