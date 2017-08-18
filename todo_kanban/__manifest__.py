0dc70e3abdf22cbc6d61ec72f065b89ac3d7092f
lam95 <lamnguyen31195@gmail.com>
2017-08-18 16:05:23 +0700

    kanban view

---
+++ todo_kanban/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'The To-Do Kanban app',
+    'description': 'Kanban model',
+    'author': 'Lam31195',
+    'depends': ['todo_ui'],
+
+    'data': [
+        #'security/ir.model.access.csv',
+        #'views/todo_view.xml',
+        #'views/todo_menu.xml',
+    ],
+}
