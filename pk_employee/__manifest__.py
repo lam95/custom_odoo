aef9fa81324f87a53f15d1222b80f53955a1687f
lam95 <lamnguyen31195@gmail.com>
2017-09-06 23:45:34 +0700

    [odoo] create custom employee module

---
+++ pk_employee/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'Extend Employee',
+    'description': 'Extend the Employee',
+    'author': 'Lam Nguyen',
+    'depends': ['hr'],
+
+    'data': [
+        'views/hr_views.xml',
+    ],
+}
